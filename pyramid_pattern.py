e4b8dfb2bafe3647f2e7de2c42e5b7f8f9c75c06
manoj-arch <manoj92067@gmail.com>
2024-08-09 13:25:40 +0530

    day5 commit

--- pyramid_pattern.py
+++ pyramid_pattern.py
@@ -1,17 +1,16 @@
-rows = int(input("Enter the rows: "))
-k = 0
+# rows = int(input("Enter the rows: "))
+# k = 0
 
-for i in range(1,rows+1):
-    for space in range(1,(rows-i)+1):
-        print(end="  ")
-    while k!=(2*i-1):
-        print("*",end=" ")
-        k+=1
-    k=0
-    print()
+# for i in range(1,rows+1):
+#     for space in range(1,(rows-i)+1):
+#         print(end="  ")
+#     while k!=(2*i-1):
+#         print(k,end=" ")
+#         k+=1
+#     k=0
+#     print()
     
 rows = int(input("Enter the rows: "))
-k = 0
 
 for i in range(1,rows+1):
     for space in range(1,(rows-i)+1):
